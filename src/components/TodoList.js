260520cb2e7186532b81be87c1967ffbccdf2dc7
omar <ropachec@uci.edu>
2021-04-11 10:17:59 -0700

    Buttons

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,14 @@
 import React from 'react';
+//import component
+import Todo from "./Todo";
 
 const TodoList = () => {
     return(
         <div className="todo-container">
-      <ul className="todo-list">
-
-      </ul>
-    </div>
+          <ul className="todo-list">
+            <Todo />
+          </ul>
+        </div>
     );
 };
 
