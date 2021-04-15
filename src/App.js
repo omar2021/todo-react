1100b38efce8ebc80e672ddf66d50a5139eb2c6f
omar <ropachec@uci.edu>
2021-04-15 10:37:15 -0700

    filtering list by completed items
    status handler

--- src/App.js
+++ src/App.js
@@ -6,14 +6,24 @@ import TodoList from './components/TodoList';
 function App() {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
   //need to filter items
   return (
     <div className="App">
       <header>
         <h1>Omar's React Todo List</h1>
       </header>
-      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
-      <TodoList setTodos={setTodos} todos={todos}/>
+      <Form 
+        inputText={inputText} 
+        todos={todos} 
+        setTodos={setTodos} 
+        setInputText={setInputText} 
+        setStatus ={setStatus} />
+      <TodoList 
+        setTodos={setTodos} 
+        todos={todos}/>
+
+
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -14,6 +14,10 @@ const Form = ({ setInputText, todos, setTodos, inputText })=>{
         ])
         setInputText("");
     };
+    const statusHandler = (e) => {
+      console.log(e.target.value);
+    };
+
 
     return(
     <form>
@@ -22,7 +26,7 @@ const Form = ({ setInputText, todos, setTodos, inputText })=>{
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
-        <select name="todos" className="filter-todo">
+        <select onChange ={statusHandler} name="todos" className="filter-todo">
           <option value="all">All</option>
           <option value="completed">Completed</option>
           <option value="uncompleted">Uncompleted</option>
